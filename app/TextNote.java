feb56fc57a5c1af8235ff3536c1f21eca7558073
Patchaya <patchaya.kha67@psru.ac.th>
2025-09-04 15:38:02 +0700

    5/6

---
+++ app/TextNote.java
@@ -0,0 +1,22 @@
+public class TextNote {
+    private  String textContent;
+
+    //getter method
+    public String getTextContent(){
+        return textContent;
+    }
+
+    //setter method
+    public void setTextContent(String newContent){
+        this.textContent = newContent;
+
+
+    }
+
+
+    //Method
+    public String  getSummary(){
+        return title+":"+textContent+"("+createdDate+")";
+        //System.out.println(title+":"+content+"("+createdDate+")");
+    }
+}
---
+++ app/src/main/java/com/example/lab06/AddNoteActivity.java
@@ -0,0 +1,116 @@
+package com.example.lab06;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Date;
+
+public class AddNoteActivity extends AppCompatActivity {
+    Button addBack;
+
+    Button addButton;
+
+    EditText title,textContent,name,id;
+
+    TextView display;
+    TextView display2;
+    TextView display1;
+
+    Button addcheck;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        addBack = findViewById(R.id.button6);//event aource
+        addBack.setOnClickListener(new View.OnClickListener() {//event listener
+            @Override
+            public void onClick(View view) {//event handler
+                System.out.println("Click!!");
+                Intent addBack = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(addBack);
+            }
+
+        });
+        addButton = findViewById(R.id.button4);
+        title = findViewById(R.id.editTextText3);
+        textContent = findViewById(R.id.editTextText4);
+        display = findViewById(R.id.textView);
+        display1 = findViewById(R.id.textView4);
+        addcheck = findViewById(R.id.button5);
+        name = findViewById(R.id.editTextText);
+       id = findViewById(R.id.editTextText2);
+        display2 = findViewById(R.id.textView5);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get data from user (EditText)
+                String strOfTitle = title.getText().toString();
+                String strOfContent = textContent.getText().toString();
+
+                String strOfDate = new Date().toString();
+                String strOfName = name.getText().toString();
+                String strOfId = id.getText().toString();
+
+
+                //set data to TextNote class
+                TextNote note1 = new TextNote();
+                note1.setTitle(strOfTitle);
+
+                note1.setTextContent(strOfContent);
+
+                note1.createdDate = strOfDate;
+                TextUser user1 = new TextUser();
+                user1.setName(strOfName);
+                user1.setId(strOfId);
+
+
+                //show note on TextView
+                display.setText(note1.getSummary());
+                display2.setText(user1.getSummary());
+            }
+        });
+        addcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strOfTitle = title.getText().toString();
+                String strOfDate = new Date().toString();
+
+                ChecklistNote note1 = new ChecklistNote();
+                note1.setTitle(strOfTitle);
+
+                note1.createdDate = strOfDate;
+
+                display1.setText(note1.getSummary());
+            }
+        });
+    }
+}
